03e9c745e1314b52f9b4eb2d804933a61bee22b7
Sanzdhiev Aldar <radlasandjiev08@gmail.com>
2025-02-24 14:02:52 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -16,59 +16,26 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        try (Session session = Util.getSession()) {
-            session.beginTransaction();
-            session.createSQLQuery("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(50), lastName VARCHAR(50), age SMALLINT)").executeUpdate();
-            session.getTransaction().commit();
-        } catch (HibernateException e) {e.printStackTrace();}
     }
 
     @Override
     public void dropUsersTable() {
-        try(Session session = Util.getSession()) {
-            session.beginTransaction();
-            session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
-            session.getTransaction().commit();
-        } catch (HibernateException e) {e.printStackTrace();}
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try (Session session = Util.getSession()) {
-            session.beginTransaction();
-            User user = new User(name, lastName, age);
-            session.save(user);
-            session.getTransaction().commit();
-        } catch (HibernateException e) {e.printStackTrace();}
     }
 
     @Override
     public void removeUserById(long id) {
-        try (Session session = Util.getSession()) {
-            session.beginTransaction();
-            User user = session.get(User.class, id);
-            session.delete(user);
-            session.getTransaction().commit();
-        }catch (HibernateException e) {e.printStackTrace();}
     }
 
     @Override
     public List<User> getAllUsers() {
-        List<User> users = new ArrayList<>();
-        try (Session session = Util.getSession()) {
-            session.beginTransaction();
-            users= session.createQuery("FROM User", User.class).list();
-            session.getTransaction().commit();
-        } catch (HibernateException e) {e.printStackTrace();}
-        return users;
+        return null;
     }
 
     @Override
     public void cleanUsersTable() {
-        try (Session session = Util.getSession()) {
-            session.beginTransaction();
-            session.createSQLQuery("DELETE FROM users").executeUpdate();
-            session.getTransaction().commit();
-        } catch (HibernateException e) {e.printStackTrace();}
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -12,36 +12,11 @@ import java.sql.SQLException;
 
 public class Util {
 
-    private static SessionFactory sessionFactory;
+    private static final String URL = "jdbc:postgresql://localhost:5432/my_database";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "Radlasand_200608";
 
-    public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            try {
-                Configuration configuration = new Configuration();
-
-                configuration.addAnnotatedClass(User.class);
-                configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect"); // Укажите диалект для вашей БД
-                configuration.setProperty("hibernate.hbm2ddl.auto", "update"); // Автоматическая генерация таблиц
-                configuration.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver"); // Драйвер
-                configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/my_database"); // URL базы данных
-                configuration.setProperty("hibernate.connection.username", "postgres"); // Имя пользователя
-                configuration.setProperty("hibernate.connection.password", "Radlasand_200608"); // Пароль
-                sessionFactory = configuration.buildSessionFactory();
-            } catch (Exception e) {e.printStackTrace();}
-        }
-        return sessionFactory;
+    public static Connection getConnection()throws SQLException {
+        return DriverManager.getConnection(URL,USER,PASSWORD);
     }
-
-    public static Session getSession() {
-        return getSessionFactory().openSession();
-    }
-
-
-//    private static final String URL = "jdbc:postgresql://localhost:5432/my_database";
-//    private static final String USER = "postgres";
-//    private static final String PASSWORD = "Radlasand_200608";
-//
-//    public static Connection getConnection()throws SQLException {
-//        return DriverManager.getConnection(URL,USER,PASSWORD);
-//    }
 }
